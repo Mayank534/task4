8d8d19b82398061ce0c14bbde4d02ae87eb5a44c
Mayank534 <mayanka22@iitk.ac.in>
2023-07-29 15:39:41 +0530

    third

--- dapp/script.js
+++ dapp/script.js
@@ -1,31 +1,47 @@
 import { ethers } from "./ethers.js";
 
-// Function to connect to MetaMask and handle the connection
-async function connectToMetaMask() {
-    if (window.ethereum) {
-      try {
-        // Request MetaMask authorization
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-  
-        // Check if MetaMask is connected
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-        if (accounts.length > 0) {
-          console.log("Successfully connected to MetaMask!");
-          window.alert("Successfully connected to MetaMask Wallet.");
-          window.location.href = "election1.html"; 
-        } else {
-          console.error("MetaMask is not connected.");
-        }
-      } catch (error) {
-        console.error("User denied account access or MetaMask is not installed.");
-      }
+const connectButton = document.getElementById('connectButton');
+const registerButton = document.getElementById('registerButton');
+
+// Check if MetaMask exists
+if (typeof window.ethereum !== 'undefined') {
+  console.log('MetaMask is installed!');
+}
+
+connectButton.onclick = async function () {
+  try {
+    // Request MetaMask to connect
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    // Check if user granted access to accounts
+    if (accounts.length === 0) {
+      alert("Please connect your MetaMask account.");
     } else {
-      console.error("Please install MetaMask to use this feature.");
+      // User is connected, redirect to election.html
+      window.alert("Successfully connected to MetaMask Wallet.");
+      window.location.href = "election.html";
     }
+  } catch (error) {
+    console.error("Error connecting to MetaMask:", error);
+    alert("An error occurred while connecting to MetaMask. Please try again.");
   }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const connectButton = document.getElementById("connectButton");
-    connectButton.addEventListener("click", connectToMetaMask);
-  });
-  
+};
+
+registerButton.onclick = async function () {
+  try {
+    // Request MetaMask to connect
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    // Check if user granted access to accounts
+    if (accounts.length === 0) {
+      alert("Please connect your MetaMask account to register.");
+    } else {
+      // User is connected, perform registration logic (e.g., store the registered account)
+      window.alert("Successfully registered with MetaMask!");
+      // You can add additional logic here, such as storing the registered account on a server or blockchain.
+    }
+  } catch (error) {
+    console.error("Error connecting to MetaMask:", error);
+    alert("An error occurred while registering with MetaMask. Please try again.");
+  }
+};
